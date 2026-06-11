5166a3882b471379ef00820d833df6eb56d7d2e9
= <larryking8118@gmail.com>
2026-06-11 11:34:13 +0000

    feat(ui): add navbar, footer and home pages

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -9,14 +9,31 @@ import styles from './navbar.module.scss'
 
 
 
+
+
+
 const Navbar = () => {
 
 
 
     return (
-        <main className={ styles.navbar }>
-            <h3>Navbar here</h3>
-        </main>
+        <nav className={ styles.navbar }>
+            <section className={ styles.navbar__brand }>
+                <h3>StayFinder</h3>
+            </section>
+
+            <section className={ styles.navbar__menu }>
+                <ul>
+                    <li>Home</li>
+                    <li>Support</li>
+                    <li>About Us</li>
+                    <li>Bookings</li>
+                    <li>
+                        <button type="button">Sign In</button>
+                    </li>
+                </ul>
+            </section>
+        </nav>
     )
 
     
