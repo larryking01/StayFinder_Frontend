97aa1ecfd820641cb19f2a689ae639cae87ee7fd
= <larryking8118@gmail.com>
2026-06-23 11:52:40 +0000

    feat(hotel-details): add hotel details page that displays information about selected hotel, including policies, pricing, description, payment methods, etc

---
+++ src/components/reviewCard/reviewCard.tsx
@@ -0,0 +1,45 @@
+import styles from './reviewCard.module.scss'
+import cover1 from '../../assets/images/hero_3.jpg'
+
+
+
+
+
+
+
+
+
+
+const ReviewCard = () => {
+
+
+    return (
+        <article className={ styles.reviewCard }>
+            <section className={ styles.authorInfo }>
+                <div className={ styles.profile }>
+                    <img src={ cover1 } />
+                    <div className={ styles.authorNameAndCountry }>
+                        <p>Larry</p>
+                        <p>Canada</p>
+                    </div>
+                </div>
+
+                <div className={ styles.timeAgo }>
+                    <p>3 weeks ago</p>
+                </div>
+
+            </section>
+
+            <section className={ styles.reviewContent }>
+                <p>
+                    Beautiful property, clean, comfortable, and well equipped. The kitchen was functional and had 
+                    everything we needed for our stay. One of the best surprises was realizing that the 
+                    balcony had a view of the CN Tower, which was a huge bonus and made...                                
+                </p>
+            </section>
+        </article>
+    )
+}
+
+
+export default ReviewCard
