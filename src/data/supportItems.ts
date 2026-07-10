46de1b1c06f2652e519e495c4f73cfe9a38e2185
= <larryking8118@gmail.com>
2026-07-10 21:54:11 +0000

    feat(support): create support page and render support items dynamically with an array

---
+++ src/data/supportItems.ts
@@ -0,0 +1,44 @@
+import { CircleUser, 
+         TriangleAlert, 
+         ShieldBan, 
+         Coins, 
+         BedDouble,
+         ShieldCheck,
+         BanknoteArrowDown
+} from 'lucide-react'
+
+
+
+
+
+
+export const supportItems = [
+    {
+        name: 'Refunds & Charges',
+        icon: BanknoteArrowDown
+    },
+    {
+        name: 'Stays',
+        icon: BedDouble
+    },
+    {
+        name: 'Account',
+        icon: CircleUser
+    },
+    {
+        name: 'Privacy',
+        icon: ShieldBan
+    },
+    {
+        name: 'Travel Alerts',
+        icon: TriangleAlert
+    },
+    {
+        name: 'Security',
+        icon: ShieldCheck
+    },
+    {
+        name: 'Loyalty & Rewards',
+        icon: Coins
+    }
+]
--- src/pages/support/support.tsx
+++ src/pages/support/support.tsx
@@ -1,6 +1,6 @@
 import styles from './support.module.scss'
-
-
+import { Search, ChevronRight } from 'lucide-react'
+import { supportItems } from '../../data/supportItems'
 
 
 
@@ -16,7 +16,41 @@ const Support = () => {
     
     return (
         <main className={ styles.support }>
-            <h3>Support</h3>
+            <section className={ styles.support__intro }>
+                <h3>Help Centre</h3>
+                <p>Hi, Traveller</p>
+            </section>
+
+
+            <section className={ styles.support__searchSupport }>
+                <div className={ styles.inputWrapper }>
+                    <input type="text" placeholder="How can we help?" />
+                    <Search size={ 22 } className={ styles.icon }/>
+                </div>
+                <button type="button" >Search</button>
+            </section>
+
+
+            <section className={ styles.support__supportArticles }>
+                <h3>Explore help articles</h3>
+                <div className={ styles.supportsGrid }>
+                    {
+                        supportItems.map(item => {
+                            let Icon = item.icon 
+
+                            return (
+                                <div className={ styles.supportItem }>
+                                    <div className={ styles.supportNameAndIcon }>
+                                        <Icon className={ styles.icon } size={ 22 }/>
+                                        <p>{ item.name }</p>
+                                    </div>
+                                    <ChevronRight size={ 25 } />
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+            </section>
         </main>
     )
 }
