ca745c1c07299bd786fcdb85a0b96650d564b327
= <larryking8118@gmail.com>
2026-07-12 15:44:43 +0000

    feat(types): generate types for filter categories, payment options, support items and ensure only lucide icons can be used for a support item

--- src/data/paymentOptions.ts
+++ src/data/paymentOptions.ts
@@ -1,16 +1,15 @@
+import type { PaymentOption } from '../types/paymentOption.model'
 import visaCardLogo from '../assets/images/visaCard.png'
 import masterCardLogo from '../assets/images/masterCard.png'
 import paypalLogo from '../assets/images/paypal.png'
 import mtnMomoLogo from '../assets/images/mtnMobileMoney.png'
 import telecelCashLogo from '../assets/images/telecelCash.jpg'
-// import combinedPaymentsLogo from '../assets/images/combinedPayments.jpg'
-// import combinedPaymentsLogo2 from '../assets/images/combinedPayments2.jpeg'
 
 
 
 
 
-export const paymentOptions = [
+export const paymentOptions: PaymentOption[] = [
     {
         name: 'Visa',
         src: visaCardLogo
---
+++ src/types/supportItem.model.ts
@@ -0,0 +1,7 @@
+// define the shape of a support item
+import type { LucideIcon } from "lucide-react"
+
+export interface SupportItem {
+    name: string,
+    icon: LucideIcon    // ensure only lucide icons can be used here
+}
