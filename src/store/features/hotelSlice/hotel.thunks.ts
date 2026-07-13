b9d3c574411ebb7470850433c30dbbb30c4ca030
= <larryking8118@gmail.com>
2026-07-13 13:45:30 +0000

    feat(env): configure environment variable with vite-env.d.ts declaration file for better typescript support, intellisense and error reduction

---
+++ src/store/features/hotelSlice/hotel.thunks.ts
@@ -0,0 +1,16 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+
+
+
+
+
+
+
+
+let api_url = import.meta.env.VITE_API_BASE_URL
+
+export const fetchHotels = createAsyncThunk('hotels/fetchHotels', async () => {
+    
+})
