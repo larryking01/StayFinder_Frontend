c67ae6c4df656826b4564dbd4f829efdd6661a7d
= <larryking8118@gmail.com>
2026-08-07 17:08:02 +0000

    feat(reservation): integrate intended destination control with reservation slice for live updates

--- src/components/locationSuggestions/locationSuggestions.tsx
+++ src/components/locationSuggestions/locationSuggestions.tsx
@@ -1,7 +1,11 @@
 import styles from './locationSuggestions.module.scss'
 import { MapPin } from 'lucide-react'
 import { useAppDispatch } from '../../hooks/useStore'
-import { toggleOpenLocationSuggestions } from '../../store/features/reservationSlice/reservation.slice'
+import { toggleOpenLocationSuggestions, setIntendedDestination } from '../../store/features/reservationSlice/reservation.slice'
+
+
+
+
 
 
 
@@ -18,15 +22,16 @@ const LocationSuggestions = () => {
     const dispatch = useAppDispatch()
 
 
-    const closeLocationSuggestions = () => {
-        dispatch(toggleOpenLocationSuggestions())
+    const handleSetIntendedDestination = () => {
+        dispatch(setIntendedDestination("Square One Shopping Center"))
+        dispatch(toggleOpenLocationSuggestions())    
     }
 
 
 
     return (
         <main className={ styles.locationSuggestions }>
-            <section className={ styles.locationSuggestions__locationItem } onClick={ closeLocationSuggestions }>
+            <section className={ styles.locationSuggestions__locationItem } onClick={ handleSetIntendedDestination }>
                 <MapPin size={ 20 }  className={ styles.icon }/>
                 <div className={ styles.location }>
                     <h3>Square One Shopping Center</h3>
