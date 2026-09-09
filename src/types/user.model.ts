fa1274761a6ed443ad4aa03f96d60f0f6814e67b
= <larryking8118@gmail.com>
2026-09-09 23:07:48 +0000

    feat(users): add thunks to handle user registering,login, profile update, logout, forgot password and reset password situations

--- src/types/user.model.ts
+++ src/types/user.model.ts
@@ -48,3 +48,25 @@ export interface User {
   updated_at: string
   is_anonymous: boolean
 }
+
+
+
+export interface CreateUserPayload {
+    firstName: string
+    lastName: string
+    email: string 
+    password: string
+}
+
+
+export interface LoginUserPayload {
+    email: string 
+    password: string
+}
+
+
+export interface UpdateUserProfilePayload {
+    firstName: string
+    lastName: string
+    email: string 
+}
